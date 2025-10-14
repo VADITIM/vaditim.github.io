dddef327809ac6ce1ff6f2b9cb59c7d7ef4610d6
VADITIM <vadim.niedental@gmail.com>
2025-10-14 17:45:29 +0200

    more changes to projects

---
+++ src/components/Work-Page/TechContainer.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="tech-container">
+			<div class="dev-container">
+				<div class="platform" :style="{ backgroundImage: `url(${projects[currentProjectIndex || 0].platform})` }"></div>
+				<div class="engine" :style="{ backgroundImage: `url(${projects[currentProjectIndex || 0].engine})` }"></div>
+				<div class="language" :style="{ backgroundImage: `url(${projects[currentProjectIndex || 0].language})` }"></div>
+			</div>
+      <div class="download"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { currentProjectIndex, projects } from '../../modules/projects';
+</script>
+
+<style lang="scss" scoped>
+@use "../../style/variables.scss" as *;
+
+.tech-container {
+  @extend .center;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 128, 0, 0.322);
+  width: 40rem;
+  height: 30rem;
+  z-index: 5;
+}
+
+.dev-container {
+	position: absolute;
+	right: 0;
+	margin: 2rem;
+  filter: drop-shadow(0px 0px 30px black);
+	perspective: 1000px;
+}
+
+.engine,
+.platform,
+.language {
+	@include rotate(-10, -20, 0);
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 6rem;
+  height: 6rem;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+	margin: 1rem;
+}
+
+.download {
+	position: absolute;
+  top: 70%;
+  left: 15%;
+  width: 6.5rem;
+  height: 6.5rem;
+	background-image: url("../../assets/images/download.png");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  filter: drop-shadow(0px 0px 30px rgb(121, 121, 121));
+}
+
+</style>
+
+
--- src/modules/projects.ts
+++ src/modules/projects.ts
@@ -6,6 +6,7 @@ export const projectsContainer = ref<HTMLElement>()
 export const isDragging = ref(false)
 export const startX = ref(0)
 export const scrollLeft = ref(0)
+export const transitioning = ref(false)
 
 
 export const projects = [
@@ -14,72 +15,100 @@ export const projects = [
     description: "Description",
     year: 1,
     img: "src/assets/images/wrathofthefallenqueen.png",
-    Engine: "src/assets/images/javascript.png",
-    Language: "src/assets/images/javascript.png",
+    engine: "src/assets/images/webengine.png",
+    language: "src/assets/images/javascript.png",
     platform: "src/assets/images/webapp.png",
+    link: "https://github.com/VADITIM/Wrath-Of-The-Fallen-Queen"
   },
   {
     name: "Haunted",
     description: "Description",
     year: 2,
     img: "src/assets/images/haunted.png",
-    Engine: "src/assets/images/unity.png",
-    Language: "C#",
-    platform: "src/assets/images/windows.png"
+    engine: "src/assets/images/unity.png",
+    language: "src/assets/images/csharp.png",
+    platform: "src/assets/images/windows.png",
+    link: "https://github.com/VADITIM/Haunted"
+  },
+	{
+    name: "Velvet Deck Web",
+    description: "Description",
+    year: 2,
+    img: "src/assets/images/velvetdeckweb.png",
+    engine: "src/assets/images/webengine.png",
+    language: "src/assets/images/javascript.png",
+    platform: "src/assets/images/webapp.png",
+    link: "https://github.com/VADITIM/Card-Game"
   },
   {
     name: "Boundless Board",
     description: "Description",
     year: 3,
     img: "src/assets/images/boundlessboard.png",
-    Engine: "src/assets/images/unity.png",
-    Language: "C#",
-    platform: "src/assets/images/windows.png"
+    engine: "src/assets/images/unity.png",
+    language: "src/assets/images/csharp.png",
+    platform: "src/assets/images/windows.png",
+    link: "https://github.com/VADITIM/Boundless-Board"
   },
   {
     name: "//NETRUNNERS",
     description: "Description",
     year: 4,
     img: "path/to/image4.jpg",
-    Engine: "src/assets/images/unity.png",
-    Language: "C#",
-    platform: "src/assets/images/windows.png"
+    engine: "src/assets/images/unity.png",
+    language: "src/assets/images/csharp.png",
+    platform: "src/assets/images/windows.png",
+    link: "https://github.com/VADITIM/NETRUNNERS"
   },
   {
     name: "Veil of Remorse",
     description: "Description",
     year: 4,
     img: "path/to/image4.jpg",
-    Engine: "src/assets/images/unity.png",
-    Language: "C#",
-    platform: "src/assets/images/windows.png"
+    engine: "src/assets/images/unity.png",
+    language: "src/assets/images/csharp.png",
+    platform: "src/assets/images/windows.png",
+    link: "https://github.com/VADITIM/Veil-of-Remorse"
   },
-    {
+	{
     name: "Simulation City - Reborn: Green Horizons",
     description: "Description",
     year: 4,
-    img: "path/to/image4.jpg",
-    Engine: "src/assets/images/unity.png",
-    Language: "C#",
-    platform: "src/assets/images/windows.png"
+    img: "src/assets/images/simulationcity.jpg",
+    engine: "src/assets/images/unity.png",
+    language: "src/assets/images/csharp.png",
+    platform: "src/assets/images/windows.png",
+    link: "https://github.com/Nobody-989/SimulationCity"
   },
-    {
-    name: "Velvet Deck",
-    description: "Description",
-    year: 4,
-    img: "src/assets/images/wrathofthefallenqueen.png",
-    Engine: "src/assets/images/godot.png",
-    Language: "C#",
-    platform: "src/assets/images/android.png"
-  },
-    {
+	{
+		name: "Velvet Deck",
+		description: "Description",
+		year: 4,
+		img: "src/assets/images/wrathofthefallenqueen.png",
+		engine: "src/assets/images/godot.png",
+		language: "src/assets/images/csharp.png",
+		platform: "src/assets/images/android.png",
+		link: "https://github.com/VADITIM/Velvet-Deck"
+	},
+	{
+		name: "Portfolio",
+		description: "Description",
+		year: 4,
+		img: "src/assets/images/portfolio.png",
+		engine: "src/assets/images/webengine.png",
+		language: "src/assets/images/typescript.png",
+		platform: "src/assets/images/webapp.png",
+		link: "https://github.com/VADITIM/Velvet-Deck"
+	},
+	{
     name: "Anomaly",
     description: "Description",
     year: 4,
     img: "src/assets/images/anomaly.JPG",
-    Engine: "src/assets/images/godot.png",
-    Language: "C#",
-    platform: "src/assets/images/windows.png"
+    engine: "src/assets/images/godot.png",
+    language: "src/assets/images/csharp.png",
+    platform: "src/assets/images/windows.png",
+    link: ""
   },
 ]
 
@@ -118,21 +147,49 @@ export function endDrag() {
   }
 }
 
+export const nextClicked = ref(false);
+export const previousClicked = ref(false);
 
+export function nextProject() {
+	if (currentProjectIndex.value >= projects.length - 1) return
+	
+  nextClicked.value = true
+  setTimeout(() => {
+    nextClicked.value = false
+  }, 500)
 
+	currentProjectIndex.value++
+	scrollToProject(currentProjectIndex.value)
+}
+
+export function previousProject() {
+	if (currentProjectIndex.value <= 0) return
+
+  previousClicked.value = true
+  setTimeout(() => {
+    previousClicked.value = false
+  }, 500)
+
+	currentProjectIndex.value--
+	scrollToProject(currentProjectIndex.value)
+}
 
 
 
 export function updateCurrentProject() {
   if (!projectsContainer.value) return
-  
+
+  transitioning.value = true
+  setTimeout(() => {
+    transitioning.value = false
+  }, 300)
+
   const scrollLeft = projectsContainer.value.scrollLeft
   
   const projectSpacing = 60 * 16
   const currentIndex = Math.floor((scrollLeft + projectSpacing / 2) / projectSpacing)
   
   currentProjectIndex.value = Math.max(0, Math.min(currentIndex, projects.length - 1))
-  console.log('Current project index:', currentProjectIndex.value, 'ScrollLeft:', scrollLeft, 'ProjectSpacing:', projectSpacing)
 }
 
 export function scrollToProject(index: number) {
