5d3cdf7483758333156088114bea71941e3edbe4
VADIM\vadim <vadim.niedental@gmail.com>
2026-02-09 16:50:10 +0100

    improved projects container

--- src/components/Work-Page/ProjectsCopy.vue
+++ src/components/Work-Page/ProjectsCopy.vue
@@ -1,29 +1,32 @@
 <template>
     <div class="copy-container" :class="{ active: activeProjectIndex !== null }">
-        <div class="project-copy" :class="{ active: activeProjectIndex !== null, cursor: clicked }" @click="closeActiveProject($event)">
-        <DescriptionGrid />
+        <div class="project-copy"
+            :class="{ active: activeProjectIndex !== null }"
+            @click="closeActiveProject($event)">
 
-        <span :class="{ active: activeProjectIndex !== null }" v-html="projects[currentProjectIndex]?.name" class="p-name"></span>
-        <span :class="{ active: activeProjectIndex !== null }" v-if="!projects[currentProjectIndex]?.wip" class="year">{{
-            projects[currentProjectIndex]?.year }}</span>
-        <span :class="{ active: activeProjectIndex !== null }" v-if="projects[currentProjectIndex]?.wip" class="wip">
-            <span>WORK IN PROGRESS <br> </span>
-            <span class="estimated" :class="{ active: activeProjectIndex !== null }">Estimated {{ projects[currentProjectIndex]?.estimated }}</span>
-        </span>
-        
-        <Transition name="notice-fade">
-            <p :class="{clicked: clicked}" v-if="projects[currentProjectIndex]?.ai && activeProjectIndex !== null" class="notice active">Visual concept generated with AI tools and refined using assets from the original game.</p>
-        </Transition>
-        <div class="project-image" :style="{ backgroundImage: `url(${projects[currentProjectIndex]?.img})` }"
-            :class="{ current: currentProjectIndex === currentProjectIndex, transitioning: transitioning }"></div>
+            <span :class="{ active: activeProjectIndex !== null }" v-html="currentProject.name" class="p-name"></span>
+            <span :class="{ active: activeProjectIndex !== null }" v-if="!currentProject.wip" class="year">{{ currentProject.year }}</span>
+            <span :class="{ active: activeProjectIndex !== null }" v-if="currentProject.wip" class="wip">
+                <span>WORK IN PROGRESS <br> </span>
+                <span class="estimated" :class="{ active: activeProjectIndex !== null }">Estimated {{ currentProject.estimated }}</span>
+            </span>
+
+            <Transition name="notice-fade">
+                <p :class="{clicked: clicked}"
+                   v-if="currentProject.ai && activeProjectIndex !== null"
+                   class="notice active">Visual concept generated with AI tools and refined using assets from the original game.</p>
+            </Transition>
+            <div class="project-image" :style="{ backgroundImage: `url(${currentProject.img})` }"></div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import DescriptionGrid from './ContentImageGrid.vue';
-import { projects, currentProjectIndex, activeProjectIndex, closeActiveProject, transitioning} from '../../modules/projects';
+import { projects, currentProjectIndex, activeProjectIndex, closeActiveProject } from '../../modules/projects';
 import { clicked } from '../../modules/techContainer';
+import { computed } from 'vue';
+
+const currentProject = computed(() => projects[currentProjectIndex.value]);
 </script>
 
 
